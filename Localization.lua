376bd4d7d4fe16e1c2e3f07a818ed9197f5104fa
Peashooter <peashooter@intenlux.com>
2016-03-02 16:18:02 -0500

    v0.000.005
    copy of local 005 to GitHub in addition to CurseForge

--- Localization.lua
+++ Localization.lua
@@ -7,8 +7,18 @@ local L, _, ns = {}, ...
 setmetatable(L, { __index = function(t, k) t[k] = k return k end })
 ns.L = L
 
--- Please feel free to suggest updates / corrections to the localizations below via 
--- http://wow.curseforge.com/addons/fishing-shack-pro/localization/
+--[[
+Localization table managed via CurseForge.
+Please feel free to suggest updates / corrections to the localizations below via 
+http://wow.curseforge.com/addons/fishing-shack-pro/localization/
+
+
+Special thanks to the following individuals for their translation assistance:
+
+German (deDE)
+pas06: http://wow.curseforge.com/profiles/pas06/
+laurenz1337: http://wow.curseforge.com/profiles/laurenz1337/
+--]]
 
 local CURRENT_LOCALE = GetLocale()
 -- English (enUS)
@@ -16,79 +26,50 @@ if CURRENT_LOCALE == "enUS" then return end
 
 -- Brazilian Portuguese (ptBR)
 if CURRENT_LOCALE == "ptBR" then
-L["Now using"] = "Agor vestindo" -- Needs review
-L["replaced by"] = "foi substituído com" -- Needs review
-L["UPGRADE!"] = "ATUALIZAÇÃO" -- Needs review
-
+--@localization(locale="ptBR", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- French (frFR)
 if CURRENT_LOCALE == "frFR" then
-L["Now using"] = "Maintenant porter" -- Needs review
-L["replaced by"] = "a été remplacé par" -- Needs review
-L["UPGRADE!"] = "SURCLASSEMENT!" -- Needs review
+--@localization(locale="frFR", format="lua_additive_table",handle-unlocalized="english")@end
 end
 
 -- German (deDE)
 if CURRENT_LOCALE == "deDE" then
-L["Now using"] = "jetzt tragen" -- Needs review
-L["replaced by"] = "wurde ersetzt durch" -- Needs review
-L["UPGRADE!"] = "VERBESSERUNG!" -- Needs review
-
+--@localization(locale="deDE", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Italian (itIT)
 if CURRENT_LOCALE == "itIT" then
-L["Now using"] = "ora indossa" -- Needs review
-L["replaced by"] = "sostituito da" -- Needs review
-L["UPGRADE!"] = "UPGRADE !" -- Needs review
-
+--@localization(locale="itIT", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Korean (koKR)
 if CURRENT_LOCALE == "koKR" then
-L["Now using"] = "Now using" -- Requires localization
-L["replaced by"] = "replaced by" -- Requires localization
-L["UPGRADE!"] = "UPGRADE!" -- Requires localization
-
+--@localization(locale="koKR", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Latin American Spanish (esMX)
 if CURRENT_LOCALE == "esMX" then
-L["Now using"] = "ahora llevaba" -- Needs review
-L["replaced by"] = "reemplazado con" -- Needs review
-L["UPGRADE!"] = "MODERNIZACIÓN" -- Needs review
-
+--@localization(locale="esMX", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Russian (ruRU)
 if CURRENT_LOCALE == "ruRU" then
-L["Now using"] = "Now using" -- Requires localization
-L["replaced by"] = "replaced by" -- Requires localization
-L["UPGRADE!"] = "UPGRADE!" -- Requires localization
-
+--@localization(locale="ruRU", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Simplified Chinese (zhCN)
 if CURRENT_LOCALE == "zhCN" then
-L["Now using"] = "Now using" -- Requires localization
-L["replaced by"] = "replaced by" -- Requires localization
-L["UPGRADE!"] = "UPGRADE!" -- Requires localization
-
+--@localization(locale="zhCN", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Spanish (esES)
 if CURRENT_LOCALE == "esES" then
-L["Now using"] = "ahora llevaba" -- Needs review
-L["replaced by"] = "fue reemplazado con" -- Needs review
-L["UPGRADE!"] = "MODERNIZACIÓN" -- Needs review
-
+--@localization(locale="esES", format="lua_additive_table",handle-unlocalized="english")@
 end
 
 -- Traditional Chinese (zhTW)
 if CURRENT_LOCALE == "zhTW" then
-L["Now using"] = "Now using" -- Requires localization
-L["replaced by"] = "replaced by" -- Requires localization
-L["UPGRADE!"] = "UPGRADE!" -- Requires localization
-
+--@localization(locale="zhTW", format="lua_additive_table",handle-unlocalized="english")@
 end
